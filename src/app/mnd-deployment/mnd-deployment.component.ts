f46215585f76b8db925b1881c22085c840327d70
joshyjoy <joshy.joy@cognicor.com>
2020-07-31 19:17:50 +0530

    mnd Deployement report completed

--- src/app/mnd-deployment/mnd-deployment.component.ts
+++ src/app/mnd-deployment/mnd-deployment.component.ts
@@ -2,23 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
+export interface MndDeployment {
+  Job_Name: string;
+  Mandays: number;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen'},
-  {position: 2, name: 'Helium'},
-  {position: 3, name: 'Lithium'},
-  {position: 4, name: 'Beryllium'},
-  {position: 5, name: 'Boron'},
-  {position: 6, name: 'Carbon'},
-  {position: 7, name: 'Nitrogen'},
-  {position: 8, name: 'Oxygen'},
-  {position: 9, name: 'Fluorine'},
-  {position: 10, name: 'Neon'},
-];
 
 @Component({
   selector: 'app-mnd-deployment',
@@ -27,12 +15,14 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class MndDeploymentComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name'];
-  dataSource = ELEMENT_DATA;
-
-  serverData: any;
-
-  startdate; enddate; startdateCmp; enddateCmp; dataSourceCmp; showCompare;
+  displayedColumns: string[] = ['Job_Name', 'Mandays'];
+  dataSource: MndDeployment[] = [{Job_Name: 'SDE', Mandays: 5}, {Job_Name: 'SDE', Mandays: 10}];
+  startdate: any;
+  enddate: any;
+  startdateCmp: any;
+  enddateCmp: any;
+  dataSourceCmp: MndDeployment[];
+  showCompare: boolean;
 
   constructor(private http: HttpClient) { }
 
@@ -43,11 +33,10 @@ export class MndDeploymentComponent implements OnInit {
     this.startdateCmp = this.startdate;
     this.enddateCmp = this.enddate;
     this.showCompare = false;
-    this.displayedColumns = ['Date', 'AreaCovered', 'Division', 'Job_Name', 'Mandays', 'Mnd/Area', 'Section_Name', 'Squad_Name'];
 
     const url = 'http://127.0.0.1:5000/mnddeploy?start=' + this.convert(this.startdate) + '&end=' + this.convert(this.enddate);
-    this.http.get(url).subscribe((data) => {
-      this.serverData = data;
+    this.http.get(url).subscribe((data: MndDeployment[]) => {
+      this.dataSource = data;
     });
   }
 
@@ -60,11 +49,30 @@ export class MndDeploymentComponent implements OnInit {
     }
   }
 
+  clickedGo() {
+    const url = 'http://127.0.0.1:5000/mnddeploy?start=' + this.convert(this.startdate) + '&end=' + this.convert(this.enddate);
+    this.http.get(url).subscribe((data: MndDeployment[]) => {
+      this.dataSource = data;
+    });
+  }
+
+  clickedCompare() {
+    this.showCompare = true;
+  }
+
+  clickedGoCompare() {
+    const url = 'http://127.0.0.1:5000/mnddeploy?start=' + this.convert(this.startdateCmp) + '&end=' + this.convert(this.enddateCmp);
+    this.http.get(url).subscribe((data: MndDeployment[]) => {
+      this.dataSourceCmp = data;
+      this.clickedCompare();
+    });
+  }
+
   convert(str) {
-		var date = new Date(str),
-			mnth = ("0" + (date.getMonth() + 1)).slice(-2),
-			day = ("0" + date.getDate()).slice(-2);
-		return [date.getFullYear(), mnth, day].join("-").toString();
-	}
+    var date = new Date(str),
+      mnth = ("0" + (date.getMonth() + 1)).slice(-2),
+      day = ("0" + date.getDate()).slice(-2);
+    return [date.getFullYear(), mnth, day].join("-").toString();
+  }
 
 }
