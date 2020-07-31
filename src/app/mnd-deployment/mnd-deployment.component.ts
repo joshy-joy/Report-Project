a2a645631abbf1b22b37633565de77f94e19c62f
joshyjoy <joshy.joy@cognicor.com>
2020-07-31 16:08:08 +0530

    mnd Deployement page on progress

--- src/app/mnd-deployment/mnd-deployment.component.ts
+++ src/app/mnd-deployment/mnd-deployment.component.ts
@@ -1,4 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen'},
+  {position: 2, name: 'Helium'},
+  {position: 3, name: 'Lithium'},
+  {position: 4, name: 'Beryllium'},
+  {position: 5, name: 'Boron'},
+  {position: 6, name: 'Carbon'},
+  {position: 7, name: 'Nitrogen'},
+  {position: 8, name: 'Oxygen'},
+  {position: 9, name: 'Fluorine'},
+  {position: 10, name: 'Neon'},
+];
 
 @Component({
   selector: 'app-mnd-deployment',
@@ -7,9 +27,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MndDeploymentComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['position', 'name'];
+  dataSource = ELEMENT_DATA;
+
+  serverData: any;
+
+  startdate; enddate; startdateCmp; enddateCmp; dataSourceCmp; showCompare;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.startdate = new Date();
+    this.enddate = new Date();
+    this.startdate.setDate(this.startdate.getDate() - 1);
+    this.startdateCmp = this.startdate;
+    this.enddateCmp = this.enddate;
+    this.showCompare = false;
+    this.displayedColumns = ['Date', 'AreaCovered', 'Division', 'Job_Name', 'Mandays', 'Mnd/Area', 'Section_Name', 'Squad_Name'];
+
+    const url = 'http://127.0.0.1:5000/mnddeploy?start=' + this.convert(this.startdate) + '&end=' + this.convert(this.enddate);
+    this.http.get(url).subscribe((data) => {
+      this.serverData = data;
+    });
   }
+
+  dateChange(type: string, event: MatDatepickerInputEvent<Date>) {
+    switch (type) {
+      case 'startdate': this.startdate = event.value; break;
+      case 'enddate': this.enddate = event.value; break;
+      case 'startdateCmp': this.startdateCmp = event.value; break;
+      case 'enddateCmp': this.enddateCmp = event.value; break;
+    }
+  }
+
+  convert(str) {
+		var date = new Date(str),
+			mnth = ("0" + (date.getMonth() + 1)).slice(-2),
+			day = ("0" + date.getDate()).slice(-2);
+		return [date.getFullYear(), mnth, day].join("-").toString();
+	}
 
 }
